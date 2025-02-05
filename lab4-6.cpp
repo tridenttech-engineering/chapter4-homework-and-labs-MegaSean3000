50b1a9b828111b0161324afb71f041c7f990d412
MegaSean3000 <sfp1216@gmail.com>
2025-02-05 18:31:40 -0500

    Update lab4-6.cpp

--- lab4-6.cpp
+++ lab4-6.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main()
 {
 	//declare variables
-	int base = 11;
-	int height = 13;
+	double base = 11.0;
+	double height = 13.0;
 	double area = 0.0;
 
 	//calculate and display area
