1a02d1035cdf39a44797420b2cd485d52572ce48
MegaSean3000 <sfp1216@gmail.com>
2025-02-05 18:56:30 -0500

    Update hw4-11.cpp

--- hw4-11.cpp
+++ hw4-11.cpp
@@ -2,10 +2,25 @@
 // Created/revised by <your name> on <current date>
 
 #include <iostream>
+
 using namespace std;
 
-int main() {
-  
+int main()
+{
+double beginning_balance = 0.0;
+double total_deposits = 0.0;
+double total_withdrawals = 0.0;
+double ending_balance = 0.0;
 
-  return 0;
-} // end of main function
+cout << "Enter the beginning balance of the account:" << endl;
+cin >> beginning_balance;
+cout << "Enter the total deposits:" << endl;
+cin >> total_deposits;
+cout << "Enter the total withdrawals" << endl;
+cin >> total_withdrawals;
+
+ending_balance = (beginning_balance + total_deposits) - total_withdrawals;
+
+cout << "End of month balance: $" << ending_balance << endl;
+    return 0;
+}
